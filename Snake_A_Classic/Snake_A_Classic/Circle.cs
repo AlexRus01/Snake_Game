07ed4594c30d5b7b56dfb0e2296130dc0b4fad45
Alexandru Rus <94389929+AlexRus01@users.noreply.github.com>
2022-12-20 17:10:39 +0200

    Add files via upload

---
+++ Snake_A_Classic/Snake_A_Classic/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Snake_A_Classic
+{
+    class Circle
+    {
+        
+            public int X { get; set; } // clasa X
+            public int Y { get; set; } // clasa Y
+
+            public Circle()
+            {
+                // Aceasta funcție restează locația șarpelui
+                X = 0;
+                Y = 0;
+            }
+    
+    }
+}
---
+++ Snake_A_Classic/Snake_A_Classic/Generic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_A_Classic
+{
+    public partial class Generic : Form
+    {
+        public Generic()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            button3.Visible = false;
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f = new Form1();
+            this.Visible = false;
+            f.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            label1.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            label1.Visible = false;
+            button3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
